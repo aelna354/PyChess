8afe607cdaa047c1f27a0a128c8f38597b4b9fc4
aelna354 <elnasrianconcept@gmail.com>
2018-07-14 06:22:37 -0600

    At first, I wasn't sure if I'd go through with this... but now I find this GitHub worthy

--- chess.py
+++ chess.py
@@ -1,46 +1,94 @@
 from tkinter import *
 
-
 class Piece():
 	def __init__(self, color, kind):
 		self.color = color
 		self.kind = kind
-		self.image = PhotoImage(file="images/"+color+kind+".png")
-		if color == "black":
-			self.direction = "up"
+		if "pawn" not in self.kind:
+			self.image = PhotoImage(file="images/"+color+kind+".png")
 		else:
-			self.direction = "black"
+			self.image = PhotoImage(file="images/"+color+kind[1:]+".png")
+			self.kind = self.kind + "prime"
 
 class Tile():
-	def __init__(self, row, column, color):
+	def __init__(self, row, col, color):
+		self.blank = {"grey": PhotoImage(file="images/greyblank.png"), "white": PhotoImage(file="images/whiteblank.png")}
 		self.row = row
-		self.column = column
+		self.col = col
 		self.color = color
-		self.index = (row-1)*8 + column
-		self.button = Button(command=lambda:self.click(self.index), bg=color)
+		self.button = Button(command=self.click(), bg=color)
 		self.piece = None
+		self.genTargets()
+
+	def genTargets(self):
+		self.targets = {}
+		verticals = []
+		horizontals = []
+		diagonals = []
+
+		for i in range(1, 9):
+			if i != self.row:
+				verticals.append((i, self.col))
+			if i != self.col:
+				horizontals.append((self.row, i))
+			for j in [(self.row-i, self.col-i), (self.row-i, self.col+i),
+			(self.row+i, self.col-i), (self.row+i, self.col+i)]:
+				if self.goodTile(j[0], j[1]):
+					diagonals.append(j)
+		self.targets["rook"] = verticals + horizontals
+		self.targets["bishop"] = diagonals
+		self.targets["queen"] = verticals + horizontals + diagonals
+
+		self.targets["knight"] = []
+		for a, b in zip([2, 2, -2, -2, 1, 1, -1, -1],
+						[1, -1, 1, -1, 2, -2, 2, -2]):
+			a = self.row + a
+			b = self.col + b
+			if self.goodTile(a, b):
+				self.targets["knight"].append((a, b))
+
+		self.targets["bpawn"] = []
+		self.targets["bpawnprime"] = []
+		self.targets["wpawn"] = []
+		self.targets["wpawnprime"] = []
+		if self.row < 8:
+			self.targets["wpawnprime"].append(((self.row+1), (self.col)))
+			self.targets["wpawn"].append(((self.row+1), (self.col)))
+			if self.row < 7:
+				self.targets["wpawnprime"].append(((self.row+2), (self.col)))
+		if self.row > 1:
+			self.targets["bpawnprime"].append(((self.row-1), (self.col)))
+			self.targets["bpawn"].append(((self.row-1), (self.col)))
+			if self.row > 2:
+				self.targets["bpawnprime"].append(((self.row-2), (self.col)))
+
+		self.targets["king"] = []
+		for i in [-1, 0, 1]:
+			for j in [-1, 0, 1]:
+				a = self.row + i
+				b = self.col + j
+				if self.goodTile(a, b):
+					self.targets["king"].append((a, b))
+
+	def goodTile(self, a, b):
+		if (a < 1) or (b < 1) or (a > 8) or (b > 8) or (self.row == a and self.col == b):
+			return False
+		return True
+
+	def getTargets(self):
+		if self.piece is not None:
+			return self.targets[self.piece.kind]
 
 	def clear(self):
-		if self.piece is not None:
-			self.piece = None
-			self.button['image'] = None
-
-	def range(self):
-		neighbors = []
-		return neighbors
-
-	def blank(self):
-		print(self.color)
-		self.button['image'] = PhotoImage(file="images/"+self.color+"blank.png")
+		self.piece = None
+		self.button['image'] = self.blank[self.color]
 
 	def placePiece(self, newPiece):
-		if self.piece is not None:
-			self.piece = None
 		self.piece = newPiece
 		self.button['image'] = self.piece.image
 
 	def click(self, index):
-		print(index)
+		print(self.getTargets())
 
 class Chess(Frame):
 	def __init__(self, master):
@@ -48,21 +96,18 @@ class Chess(Frame):
 		self.tiles = {}
 		for i in range(1, 9):
 			for j in range(1, 9):
-				print(i + j)
 				if (i + j) % 2 == 0:
 					self.tiles[(i, j)] = Tile(i, j, "white")
 				else:
 					self.tiles[(i, j)] = Tile(i, j, "grey")
-			self.tiles[(i, j)].button.grid(row=i, column=j)
 		self.initBoard()
 		for key, val in self.tiles.items():
-			self.tiles[key].button.grid(row=key[0], column=key[1])
-
+			val.button.grid(row=key[0], column=key[1])
 
 	def initBoard(self):
 		for i in range(1, 9):
-			self.tiles[2, i].placePiece(Piece("white", "pawn"))
-			self.tiles[7, i].placePiece(Piece("black", "pawn"))
+			self.tiles[2, i].placePiece(Piece("white", "wpawn"))
+			self.tiles[7, i].placePiece(Piece("black", "bpawn"))
 		for i in [2, 7]:
 			self.tiles[1, i].placePiece(Piece("white", "knight"))
 			self.tiles[8, i].placePiece(Piece("black", "knight"))
@@ -78,7 +123,7 @@ class Chess(Frame):
 		self.tiles[8, 5].placePiece(Piece("black", "queen"))
 		for i in [3, 4, 5, 6]:
 			for j in range(1, 9):
-				self.tiles[i, j].blank()
+				self.tiles[i, j].clear()
 
 program = Tk()
 program.title("YouTube-DL Basic GUI")
